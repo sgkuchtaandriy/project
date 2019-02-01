dd9127fe747ec053d0b48613c7227fc69e3fdb4a
Dev <sg.kuchta.andriy@gmail.com>
2019-02-01 19:00:21 +0200

    click

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,18 @@
 import React from 'react';
 export default class Counter extends React.Component{
     render(){
+        constructor(){
+            super();
+            this.state = {
+                correntNumber: 0,
+
+            };
+        }
         return(
             <div>
-                <span>0</span>
-                <button>Increment</button>
+                <span> {this.state.correntNumber} </span>
+                <button onClick={() => this.increment()}>Increment</button>
+
             </div>
         )
     }
